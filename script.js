76d87cbca8ccd4f2f8639e062dd91e45299b6120
BasheerMohamed <Basheer_Mohamed@proton.me>
2024-09-02 12:12:02 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function clearForm() {
+    // Clear all text input boxes
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("mobile").value = "";
+    document.getElementById("pass").value = "";
+    document.getElementById("cp").value = "";
+}
